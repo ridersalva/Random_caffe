1a5b2b6c987c1f255c98605659b7418311eac120
salva <saersalva@gmail.com>
2022-07-14 23:02:23 +0200

    getting some back ready and some front

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react'
+import { Navbar as Nav } from 'react-bootstrap'
+
+import '../css/Navbar.css'
+
+function Navbar() {
+
+  return (
+
+    <>
+      <Nav className='Navv'>
+      
+      </Nav>
+
+    </>
+
+
+  )
+}
+export default Navbar
