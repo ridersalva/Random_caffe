84c96f38e707e7d6b52c5a823c103b2711ff46bd
Salvador Rus Sanchez <salvador.rus@mecalux.com>
2022-07-18 13:15:09 +0200

    starting pahse 2

--- src/components/Logo_cafe.jsx
+++ src/components/Logo_cafe.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Image } from 'react-bootstrap'
 import '../css/Logo_cafe.css'
+import cafe_logo from '../img/reshot-icon-coffee-break-UR59EH28FN.svg'
 
  function Logo_cafe() {
   return (
     <div className='logo'>
-
-
+        <Image src={cafe_logo} alt='cafe logo' />
     </div>
   )
 }
